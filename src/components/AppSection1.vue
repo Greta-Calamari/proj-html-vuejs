4eaded4430d35eb07524b99bf73ef15adb0473b9
ladykakki <greta_calamari@hotmail.it>
2022-05-16 13:26:36 +0200

    start first section

--- src/components/AppSection1.vue
+++ src/components/AppSection1.vue
@@ -1,7 +1,34 @@
 <template>
-    <div>
+    <div class="container c-color">
+        <p class="text-center pt-5">Stimulated to learn?</p>
+        <h5 class="title text-center">Featured Online Courses</h5>
+
+        <div class="row">
+            <div class="col-6">
+                
+            </div>
+            <div class="col-6"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+        
+        <div class="row">
+            <div class="col-6"></div>
+            <div class="col-6"></div>
+        </div>
+        
+        <!-- <button type="button" class="btn my-3 text-center">
+                  
+                
+        </button> -->
 
     </div>
+        
+
+
 </template>
 
 <script>
@@ -13,5 +40,23 @@ export default {
 
 <style lang="scss" scoped>
 @import '../styles/vars.scss';
+h5{
+    font-size: 30px;
+}
+.c-color{
+    background-color: $bg;
+
+}
+.title{
+    color: $violet;
+}
+
+.btn{
+    background: $green !important; 
+    color: $white !important;
+    padding: 15px !important;
+    
+
+}
 
 </style>
