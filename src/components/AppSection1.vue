9c019f867ebfbcd25c93393623e8e77b7081bf3f
ladykakki <greta_calamari@hotmail.it>
2022-05-16 14:54:38 +0200

    first section done

--- src/components/AppSection1.vue
+++ src/components/AppSection1.vue
@@ -1,29 +1,136 @@
 <template>
     <div class="container c-color">
-        <p class="text-center pt-5">Stimulated to learn?</p>
-        <h5 class="title text-center">Featured Online Courses</h5>
+        <p class="text-center pt-5 mb-1">Stimulated to learn?</p>
+        <h4 class="title text-center mb-5 fs-2">Featured Online Courses</h4>
+<div class="d-flex flex-wrap justify-content-around">
 
-        <div class="row">
-            <div class="col-6">
-                
-            </div>
-            <div class="col-6"></div>
-        </div>
+        <div class="mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                   <img src="../assets/images/course-02-480x298.jpg" class="img-fluid circle" alt="...">
+                </div>
+                 <div class="col-md-8">
+                   <div class="card-body ms-2">
+                   <h5 class="card-title price">$40.<span class="mini">00</span></h5>
+                   <p class="card-text tit-ca">Learning to Write as a Professional Author</p>
+                   <p class="card-text tit-c"><small class="">
+                       <i class="fa-solid fa-file-lines"></i>
+                       20 Lessons
+                       <i class="fa-solid fa-user ms-4"></i>
+                       50 Students
 
-        <div class="row">
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-        </div>
-        
-        <div class="row">
-            <div class="col-6"></div>
-            <div class="col-6"></div>
-        </div>
-        
-        <!-- <button type="button" class="btn my-3 text-center">
-                  
+                       </small></p>
+                 </div>
+               </div>
+       </div>
+   </div>
+   <div class="mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                   <img src="../assets/images/stock-full-hd-03-480x298.jpg" class="img-fluid circle" alt="...">
+                </div>
+                 <div class="col-md-8">
+                   <div class="card-body ms-2">
+                   <h5 class="card-title price">Free</h5>
+                   <p class="card-text tit-ca">Customer-centric info-Tech Strategies</p>
+                   <p class="card-text tit-c"><small class="">
+                       <i class="fa-solid fa-file-lines"></i>
+                       20 Lessons
+                       <i class="fa-solid fa-user ms-4"></i>
+                       50 Students
+
+                       </small></p>
+                 </div>
+               </div>
+       </div>
+   </div>
+   <div class=" mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                   <img src="../assets/images/stock-full-hd-04-480x298.jpg" class="img-fluid circle" alt="...">
+                </div>
+                 <div class="col-md-8">
+                   <div class="card-body ms-2">
+                   <h5 class="card-title">$19.<span class="mini">00</span></h5>
+                   <p class="card-text tit-ca">Open Programming Courses for Everyone:Python</p>
+                   <p class="card-text tit-c"><small class="">
+                       <i class="fa-solid fa-file-lines"></i>
+                       20 Lessons
+                       <i class="fa-solid fa-user ms-4"></i>
+                       50 Students
+
+                       </small></p>
+                 </div>
+               </div>
+       </div>
+   </div>
+   <div class=" mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                   <img src="../assets/images/stock-full-hd-06-480x298.jpg" class="img-fluid circle" alt="...">
+                </div>
+                 <div class="col-md-8">
+                   <div class="card-body ms-2">
+                   <h5 class="card-title">$26.<span class="mini">00</span></h5>
+                   <p class="card-text tit-ca">Academic Listening and Note-taking</p>
+                   <p class="card-text tit-c"><small class="">
+                       <i class="fa-solid fa-file-lines"></i>
+                       20 Lessons
+                       <i class="fa-solid fa-user ms-4"></i>
+                       50 Students
+
+                       </small></p>
+                 </div>
+               </div>
+       </div>
+   </div>
+   <div class="mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                   <img src="../assets/images/course-featured-image-01-480x298.jpg" class="img-fluid circle" alt="...">
+                </div>
+                 <div class="col-md-8">
+                   <div class="card-body ms-2">
+                   <h5 class="card-title">$39.<span class="mini">00</span></h5>
+                   <p class="card-text tit-ca">Master jQuery in a Short Period of Time</p>
+                   <p class="card-text tit-c"><small class="">
+                       <i class="fa-solid fa-file-lines"></i>
+                       20 Lessons
+                       <i class="fa-solid fa-user ms-4"></i>
+                       50 Students
+
+                       </small></p>
+                 </div>
+               </div>
+       </div>
+   </div>
+   <div class=" mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                   <img src="../assets/images/stock-full-hd-05-480x298.jpg" class="img-fluid circle" alt="...">
+                </div>
+                 <div class="col-md-8">
+                   <div class="card-body ms-2">
+                   <h5 class="card-title">$59.<span class="mini">00</span> </h5>
+                   <p class="card-text tit-ca">Introduction to Javascript for Beginners</p>
+                   <p class="card-text tit-c"><small class="">
+                       <i class="fa-solid fa-file-lines "></i>
+                       20 Lessons
+                       <i class="fa-solid fa-user ms-4"></i>
+                        50 Students
+
+                       </small></p>
+                 </div>
+               </div>
+       </div>
+  </div>
+</div>
+
+        <button type="button" class="btn my-3 ">
+                 View all courses
+                 <i class="fa-solid fa-arrow-right-long"></i> 
                 
-        </button> -->
+        </button>
 
     </div>
         
@@ -41,7 +148,9 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/vars.scss';
 h5{
-    font-size: 30px;
+    font-size: 20px;
+    font-weight: 700;
+    color: $green;
 }
 .c-color{
     background-color: $bg;
@@ -52,11 +161,37 @@ h5{
 }
 
 .btn{
+    
+    width: 20% !important;
     background: $green !important; 
     color: $white !important;
     padding: 15px !important;
+    margin: 5% 40% !important;
     
+}
+
+.circle{
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+
+.mini{
+    font-size: 15px;
 
 }
+.tit-ca{
+    font-size: 20px;
+    font-weight: 700;
+    color: $violet;
+}
+
+.tit-c{
+    color: $violet;
+   
+}
+   
+
 
 </style>
