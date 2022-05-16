f6f1b3627fae7f4b96858f643598d93d24d3f762
ladykakki <greta_calamari@hotmail.it>
2022-05-16 10:31:24 +0200

    scaffolding and install npm's packages

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header>
+      <app-header/>
+    </header>
+   
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<style lang="scss">
+@import './styles/general.scss';
+</style>
